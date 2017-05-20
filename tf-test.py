fc9e3b3fa35b3ec4b28aaf0040b449e4b6af2a06
jarvis-owl <jarvis.owl@googlemail.com>
2017-05-20 13:12:15 +0200

    first tensorflow test file

---
+++ tf-test.py
@@ -0,0 +1,19 @@
+
+import tensorflow as tf
+
+#tried to silent tf - unsuccessfully
+#export TF_CPP_MIN_LOG_LEVEL=2
+#tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+
+x1 = tf.constant([5])
+x2 = tf.constant([6])
+
+
+result = tf.multiply(x1,x2)
+print (result)
+
+
+sess = tf.Session()
+print(sess.run(result))
