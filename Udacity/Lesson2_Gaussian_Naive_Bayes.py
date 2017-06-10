411ae63c3e17c4866d6adb1f8c5b0749b89aa836
jarvis-owl <jarvis.owl@googlemail.com>
2017-06-10 22:35:22 +0200

    init udacity folder - migth become obsolete quickly

--- Udacity/Lesson2_Gaussian_Naive_Bayes.py
+++ Udacity/Lesson2_Gaussian_Naive_Bayes.py
@@ -8,6 +8,7 @@ import numpy as np
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 Y = np.array([1, 1, 1, 2, 2, 2])
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 clf = GaussianNB()
 clf.fit(X, Y)
 
@@ -17,3 +18,6 @@ clf_pf = GaussianNB()
 clf_pf.partial_fit(X, Y, np.unique(Y))
 
 print(clf_pf.predict([[-0.8, -1]]))
+
+#pred = clf.predict([-0.7,-0.9])
+#print(accuracy_score(X,pred))
--- math-functions/function_1.py
+++ math-functions/function_1.py
@@ -4,7 +4,11 @@ import numpy as np
 import matplotlib as plt
 import math
 import random
+import time
 
+t1 = time.time()
 t = np.linspace(1,10,100)
 u = random.randint(1,10)
+time.sleep(5)
 print(t)
+print(time.time()-t1)
